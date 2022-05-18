b683d2d3a194816910b5f5089eac6f6495cd18da
IgorSMendes <igor07082001@gmail.com>
2022-05-18 12:37:07 -0300

    Atualização

---
+++ Java-Collections/TestandoListas2.java
@@ -0,0 +1,13 @@
+public class TestandoListas2 {
+    public static void main(String[] args) {
+    String aula1="Conhecendo mais de listas";
+    String aula2="Modelando a classe Aula";
+    String aula3="Trabalhando com cursos e sets";
+
+    ArrayList<String> aulas = new ArrayList<>();
+    aulas.add(aula1);
+    aulas.add(aula2);
+    aulas.add(aula3); 
+    System.out.println(aulas);  
+    }
+}
